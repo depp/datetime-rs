4db0556feeead5e1325bd16555dabf64c54672d2
Dietrich Epp <depp@zdome.net>
2014-06-07 23:54:21 -0700

    Add duration formatting and parsing

--- src/lib/duration.rs
+++ src/lib/duration.rs
@@ -1,5 +1,6 @@
-use std::num::Bounded;
-use std::io::{MemWriter, BufWriter, IoResult};
+use std::num::{Bounded, pow};
+use std::u64;
+use std::io::{MemWriter, IoResult};
 use std::fmt::{Show, Formatter, FormatError, WriteError};
 use std::fmt::rt::AlignLeft;
 use std::from_str::FromStr;
@@ -82,7 +83,105 @@ impl Bounded for Duration {
 
 impl FromStr for Duration {
     fn from_str(s: &str) -> Option<Duration> {
-        None
+        // We accept strings of the format "PT<N>S", where <N> is a decimal
+        // number, possibly negative, possibly using a comma, and the
+        // remaining characters are case-insensitive.
+
+        let minval: i64 = Bounded::min_value();
+        let maxval: i64 = Bounded::max_value();
+
+        let r = s;
+        let r = match r.slice_shift_char() {
+            (Some(c), r) => if c == 'P' || c == 'p' { r } else { return None },
+            _ => return None,
+        };
+        let r = match r.slice_shift_char() {
+            (Some(c), r) => if c == 'T' || c == 't' { r } else { return None },
+            _ => return None
+        };
+
+        let (negative, r) = match r.slice_shift_char() {
+            (Some(c), rem) => if c == '-' { (true, rem) } else { (false, r) },
+            _ => return None
+        };
+
+        let (sec_part, r) = {
+            let len = match r.find(|c: char| !(c >= '0' && c <= '9')) {
+                Some(i) => i, None => return None
+            };
+            if len == 0 {
+                return None;
+            }
+            let n = match from_str::<u64>(r.slice_to(len)) {
+                Some(n) => n, None => return None
+            };
+            let n = match n.checked_mul(&(SECOND.ticks as u64)) {
+                Some(n) => n,
+                _ => return None,
+            };
+            (n, r.slice_from(len))
+        };
+
+        let (tick_part, r) = match r.slice_shift_char() {
+            (Some(c), rem) => if c == '.' || c == ',' {
+                let len = match rem.find(|c: char| !(c >= '0' && c <= '9')) {
+                    Some(i) => i, None => return None
+                };
+                if len == 0 {
+                    return None;
+                }
+                let tick_part = if len <= 7 {
+                    match from_str::<u64>(rem.slice_to(len)) {
+                        Some(n) => n * pow(10u64, 7 - len),
+                        None => return None,
+                    }
+                } else {
+                    let n = match from_str::<u64>(rem.slice_to(7)) {
+                        Some(n) => n,
+                        None => return None
+                    };
+                    if rem.char_at(7) == '5' &&
+                        rem.slice_from(7).chars().all(|c| c == '0') {
+                        n + (n & 1)
+                    } else if rem.char_at(7) >= '5' {
+                        n + 1
+                    } else {
+                        n
+                    }
+                };
+                (tick_part, rem.slice_from(len))
+            } else {
+                (0u64, r)
+            },
+            _ => (0u64, r)
+        };
+
+        match r.slice_shift_char() {
+            (Some(c), r) => if (c == 'S' || c == 's') && r.is_empty() {
+            } else {
+                return None
+            },
+            _ => return None
+        }
+
+        let ticks = match sec_part.checked_add(&tick_part) {
+            Some(n) => n,
+            _ => return None,
+        };
+
+        if negative {
+            if ticks > minval as u64 {
+                None
+            } else {
+                Some(Duration { ticks: -(ticks as i64) })
+            }
+        } else {
+            if ticks > maxval as u64 {
+                None
+            } else {
+                Some(Duration { ticks: ticks as i64 })
+            }
+        }
     }
 }
 
@@ -129,54 +228,56 @@ impl Duration {
     fn to_utf8_io(&self, precision: Option<uint>) -> IoResult<Vec<u8>> {
         let mut w = MemWriter::with_capacity(32);
         try!(w.write_str("PT"));
-        let mag = if self.ticks >= 0 {
-            self.ticks as u64
+        let (negative, mag) = if self.ticks >= 0 {
+            (false, self.ticks as u64)
         } else {
-            try!(w.write_char('-'));
-            -self.ticks as u64
+            (true, -self.ticks as u64)
         };
-        let secs = mag / (SECOND.ticks as u64);
-        let ticks = mag % (SECOND.ticks as u64);
-        try!(write!(&mut w, "{:u}", secs));
-        match precision {
-            Some(prec) => {
-                if prec > 0 {
-                    try!(w.write_char('.'));
-                    let mut buf = [0u8, ..7];
-                    {
-                        let mut bw = BufWriter::new(buf);
-                        try!(write!(&mut bw, "{:07u}", ticks));
-                    }
-                    if prec <= 7 {
-                        try!(w.write(buf.slice(0, prec)));
-                    } else {
-                        try!(w.write(buf));
-                        for _ in range(0, prec - 7) {
-                            try!(w.write_char('0'));
-                        }
-                    }
-                }
-            },
+        let (significand, dotpos, zeroes) = match precision {
             None => {
-                if ticks > 0 {
-                    let mut buf = [0u8, ..7];
-                    {
-                        let mut bw = BufWriter::new(buf);
-                        try!(write!(&mut bw, "{:07u}", ticks));
+                let mut significand: u64 = mag;
+                let mut dotpos: uint = 7;
+                while dotpos > 0 && (significand % 10) == 0 {
+                    significand /= 10;
+                    dotpos -= 1;
+                }
+                (significand, dotpos, 0u)
+            },
+            Some(prec) => {
+                if prec < 7 {
+                    let dotpos = if prec > 0 { prec } else { 0 };
+                    let x = pow(10u64, 7 - dotpos);
+                    let mut ival = mag / x;
+                    let rem = mag % x;
+                    if rem > x / 2 || (rem == x / 2 && (ival & 1) == 1) {
+                        ival += 1;
                     }
-                    let mut prec = 0;
-                    for i in range(0, 7u) {
-                        if buf[i] != '0' as u8 {
-                            prec = i + 1;
-                        }
-                    }
-                    if prec > 0 {
-                        try!(w.write_char('.'));
-                        try!(w.write(buf.slice(0, prec)));
-                    }
+                    (ival, dotpos, 0u)
+                } else {
+                    (mag, 7, prec - 7)
                 }
             }
         };
+        if negative && significand > 0 {
+            try!(w.write_char('-'));
+        }
+        try!(u64::to_str_bytes(significand, 10, |v| {
+            if v.len() <= dotpos {
+                try!(w.write_str("0.0000000".slice_to(2 + dotpos - v.len())));
+                try!(w.write(v));
+            } else {
+                try!(w.write(v.slice_to(v.len() - dotpos)));
+                if dotpos > 0 {
+                    try!(w.write_char('.'));
+                    try!(w.write(v.slice_from(v.len() - dotpos)));
+                }
+            }
+            for _ in range(0, zeroes) {
+                try!(w.write_char('0'));
+            }
+            Ok(())
+        }));
+        try!(w.write_char('S'));
         Ok(w.unwrap())
     }
 
@@ -214,4 +315,135 @@ impl Duration {
     pub fn from_standard_weeks(n: i64) -> Duration {
         STANDARD_WEEK * n
     }
+}
+
+#[cfg(test)]
+fn test_format_1(d: i64, s: &str) -> () {
+    let out = format!("{}", Duration { ticks: d });
+    if out.as_slice() != s {
+        fail!("input: {}, expected: '{}', output: '{}'",
+              d, s, out);
+    }
+}
+
+#[test]
+fn test_format() {
+    test_format_1(0, "PT0S");
+    test_format_1(1, "PT0.0000001S");
+    test_format_1(-1, "PT-0.0000001S");
+    test_format_1(10, "PT0.000001S");
+    test_format_1(-10, "PT-0.000001S");
+    test_format_1(100, "PT0.00001S");
+    test_format_1(-100, "PT-0.00001S");
+    test_format_1(1000, "PT0.0001S");
+    test_format_1(-1000, "PT-0.0001S");
+    test_format_1(10000, "PT0.001S");
+    test_format_1(-10000, "PT-0.001S");
+    test_format_1(100000, "PT0.01S");
+    test_format_1(-100000, "PT-0.01S");
+    test_format_1(1000000, "PT0.1S");
+    test_format_1(-1000000, "PT-0.1S");
+
+    test_format_1(SECOND.ticks, "PT1S");
+    test_format_1(-SECOND.ticks, "PT-1S");
+
+    test_format_1(MINUTE.ticks, "PT60S");
+    test_format_1(-MINUTE.ticks, "PT-60S");
+
+    test_format_1(HOUR.ticks, "PT3600S");
+    test_format_1(-HOUR.ticks, "PT-3600S");
+
+    test_format_1(STANDARD_DAY.ticks, "PT86400S");
+    test_format_1(-STANDARD_DAY.ticks, "PT-86400S");
+
+    test_format_1(STANDARD_WEEK.ticks, "PT604800S");
+    test_format_1(-STANDARD_WEEK.ticks, "PT-604800S");
+
+    test_format_1(Bounded::max_value(), "PT922337203685.4775807S");
+    test_format_1(Bounded::min_value(), "PT-922337203685.4775808S");
+}
+
+#[cfg(test)]
+fn test_roundtrip_1(n: Duration) {
+    let s = format!("{}", n);
+    match from_str::<Duration>(s.as_slice()) {
+        None => fail!("cannot parse: {}", s),
+        Some(m) => assert_eq!(n, m),
+    };
+}
+
+#[test]
+fn test_roundtrip() {
+    test_roundtrip_1(Duration { ticks: 0 });
+    test_roundtrip_1(Duration { ticks: 1 });
+    test_roundtrip_1(Duration { ticks: -1 });
+    test_roundtrip_1(Duration { ticks: 9999999 });
+    test_roundtrip_1(Duration { ticks: -9999999 });
+    test_roundtrip_1(Duration { ticks: 10000000 });
+    test_roundtrip_1(Duration { ticks: -10000000 });
+    test_roundtrip_1(Duration { ticks: 1234567890 });
+    test_roundtrip_1(Duration { ticks: -1234567890 });
+    test_roundtrip_1(Bounded::min_value());
+    test_roundtrip_1(Bounded::max_value());
+}
+
+#[cfg(test)]
+fn test_rounding_1(prec: uint, d: i64, s: &str) {
+    let out = format!("{:.*}", prec, Duration { ticks: d });
+    if out.as_slice() != s {
+        fail!("precision: {}, input: {}, expected: '{}', output: '{}'",
+              prec, d, s, out);
+    }
+}
+
+#[test]
+fn test_rounding() {
+    test_rounding_1(0, 0, "PT0S");
+    test_rounding_1(1, 0, "PT0.0S");
+    test_rounding_1(2, 0, "PT0.00S");
+    test_rounding_1(3, 0, "PT0.000S");
+    test_rounding_1(4, 0, "PT0.0000S");
+    test_rounding_1(5, 0, "PT0.00000S");
+    test_rounding_1(6, 0, "PT0.000000S");
+    test_rounding_1(7, 0, "PT0.0000000S");
+    test_rounding_1(8, 0, "PT0.00000000S");
+    test_rounding_1(9, 0, "PT0.000000000S");
+
+    test_rounding_1(5,  50, "PT0.00000S");
+    test_rounding_1(5,  51, "PT0.00001S");
+    test_rounding_1(5, 149, "PT0.00001S");
+    test_rounding_1(5, 150, "PT0.00002S");
+
+    test_rounding_1(3, 59984999, "PT5.998S");
+    test_rounding_1(3, 59985000, "PT5.998S");
+    test_rounding_1(3, 59994999, "PT5.999S");
+    test_rounding_1(3, 59995000, "PT6.000S");
+
+    test_rounding_1(2, -50000, "PT0.00S");
+    test_rounding_1(2, -50001, "PT-0.01S");
+}
+
+#[cfg(test)]
+fn test_parsefail_1(s: &str) {
+    match from_str::<Duration>(s) {
+        None => (),
+        Some(_) => fail!("input: '{}'", s),
+    }
+}
+
+#[test]
+fn test_parsefail() {
+    test_parsefail_1("PT.S");
+    test_parsefail_1("PT.0S");
+    test_parsefail_1("PT0.S");
+    test_parsefail_1("PTS");
+    test_parsefail_1("PT-S");
+    test_parsefail_1("PT-.S");
+    test_parsefail_1("PT-0.S");
+    test_parsefail_1("PT-.0S");
+    test_parsefail_1("AT0S");
+    test_parsefail_1("PU0S");
+    test_parsefail_1("P0S");
+    test_parsefail_1("T0S");
+    test_parsefail_1("PT0");
 }
